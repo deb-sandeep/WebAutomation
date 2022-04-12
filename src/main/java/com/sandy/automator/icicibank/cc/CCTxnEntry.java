440c0f5d068f4c792dbe7eb29194796966875940
Sandeep Deb <deb.sandeep@gmail.com>
2022-04-12 18:10:16 +0530

    Updates

--- src/main/java/com/sandy/automator/icicibank/cc/CCTxnEntry.java
+++ src/main/java/com/sandy/automator/icicibank/cc/CCTxnEntry.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat ;
 import java.text.SimpleDateFormat ;
 import java.util.Date ;
 
-import org.apache.commons.lang3.StringUtils ;
+import org.apache.commons.lang.StringUtils ;
 
 public class CCTxnEntry {
     
